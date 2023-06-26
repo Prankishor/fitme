7179959ea368c60644591b472c339d0a835ab09d
Prankishor <33565966+Prankishor@users.noreply.github.com>
2023-06-26 12:24:57 +0530

    Developed landing page part 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,12 @@ const Home = () => {
         <div className='home_container'>
             <div className='home_left'>
                 <div className='hero_line'>
-                    <h1>Having issues with weightloss? Trying to get fit?<br></br> Well, FitMe is here to help you achieve your dream physique.</h1>
+                    <h1 className='tagline'> GET FIT WITH FITME </h1>
+                    <h1 className='tagdesc'>Having issues with weightloss and overeating? Trying to get fit? Looking for guidance? <br></br>FitMe is here to help you achieve your dream physique.</h1>
                 </div>
                 <div className='btns'>
-                    <button className='chat'>Chat with us</button>
-                    <button className='knowmore'>Know More</button>
+                    <a href="#chat"><button className='chat'>Chat with us</button></a>
+                    <a href="#know"><button className='knowmore'>Know More</button></a>
                 </div>
             </div>
             <div className='hero_image'>
