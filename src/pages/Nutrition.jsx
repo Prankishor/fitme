77fd13bdafbcc40069494917688472650aedc8ef
Prankishor <33565966+Prankishor@users.noreply.github.com>
2023-08-06 01:41:22 +0530

    Added component in Nutrition page

--- src/pages/Nutrition.jsx
+++ src/pages/Nutrition.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Nutrition = () => {
     return (
         <div>
-            <h1>Nutrition!!</h1>
+            <div className='quote_container'>
+                <span className='quote'>Optimum nutrition is the medicine of tomorrow.</span>
+                <br></br>
+                <span className='author'>Linus Pauling</span>
+            </div>
+
+            <div className='search_container'>
+                <input type="text" className='search' name="search" placeholder='Enter a food item'></input>
+                <button className='searchbtn'>Search</button>
+            </div>
         </div>
     )
 }
